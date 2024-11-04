ebb3eb31c1b00e4fcafeb45a2243cc17edbfea31
Peikkohi <peik.kalliokoski@helsinki.fi>
2024-11-04 21:36:51 +0200

    fix varasto tilavuus

--- src/varasto.py
+++ src/varasto.py
@@ -5,7 +5,6 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        self.tilavuus = 3
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
